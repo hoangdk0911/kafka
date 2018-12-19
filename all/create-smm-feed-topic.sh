6455730ecec4cffb118881fa2999c783895ea747
Unknown <hoangdk0911@gmail.com>
2018-12-19 10:58:34 +0700

    change command create feed topic

--- all/create-smm-feed-topic.sh
+++ all/create-smm-feed-topic.sh
@@ -2,4 +2,4 @@ docker run \
   --net=host \
   --rm \
   confluentinc/cp-kafka \
-  kafka-topics --create --topic smm-feed --partitions 20 --replication-factor 3 --if-not-exists --zookeeper localhost:32181 --alter --add-config retention.ms=3600000
+  kafka-topics --create --topic smm-feed --partitions 20 --replication-factor 3 --if-not-exists --zookeeper localhost:32181
